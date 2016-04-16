548ac9783766213670d9689471567028f8e03abb
cruor99 <kliknes@gmail.com>
2016-04-16 10:24:47 +0200

    it works now

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ logging.basicConfig(level=logging.DEBUG)
 from plyer import notification
 import sys
 
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import requests
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 
 class HeartbeatRoot(BoxLayout):
     newstatus = StringProperty("")
@@ -22,7 +26,7 @@ class HeartbeatRoot(BoxLayout):
     client_token = StringProperty(
         "6rgcw2zlr4ubpvcegjajqpnmehx5gp5zm1yigjzp1mgfvy6c")
     pushurl = StringProperty(
-        "http://1dvxtg49adq5f5jtzm2a04p2sr2pje3fem1x6gfu2cyhr30p.pushould.com")
+        "https://1dvxtg49adq5f5jtzm2a04p2sr2pje3fem1x6gfu2cyhr30p.pushould.com")
 
     def __init__(self, **kwargs):
         super(HeartbeatRoot, self).__init__(**kwargs)
@@ -30,24 +34,25 @@ class HeartbeatRoot(BoxLayout):
 
     def start_thread(self, *args):
         self.bind(newstatus=self.do_notify)
+        print self.client_token
         pushthread = Thread(target=self.check_pushould)
         pushthread.daemon = True
         pushthread.start()
 
     def check_pushould(self):
-        socketio = SocketIO(self.pushurl,
-                            params={"client_token": self.client_token},
-                            verify=False)
+        socketio = SocketIO(
+            self.pushurl,
+            params={"client_token": str(self.client_token)},
+            verify=False)
         socketio.on("send", self.add_status)
-        socketio.emit("subscribe",
-                      {"room:": "TestSecurity"})
+        socketio.emit("subscribe", {"room": "TestSecurity"})
         socketio.wait()
 
     def add_status(self, response):
-        self.newstatus = response["statusmessage"]
-        self.statuscode = response["statuscode"]
-        self.devicename = response["devicename"]
-        self.deviceid = response["deviceid"]
+        self.newstatus = str(response["statusmessage"])
+        self.statuscode = str(response["statuscode"])
+        self.devicename = str(response["devicename"])
+        self.deviceid = str(response["deviceid"])
         print response
         print self.newstatus
         sys.stdout.flush()
